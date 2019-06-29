358cfddb63eb49dbe56e7f2dcdc58a44785ab4e2
Alex Perez-Pujol <alexperez@paradigmadigital.com>
2019-06-29 11:41:48 +0200

    Upgrade to terraform 12

--- main.tf
+++ main.tf
@@ -1,28 +1,29 @@
 # Create VPC
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.aws_vpc_cidr}"
-  enable_dns_hostnames = "${var.aws_vpc_enable_dns_hostnames}"
-  enable_dns_support = "${var.aws_vpc_enable_dns_support}"
+  cidr_block           = var.aws_vpc_cidr
+  enable_dns_hostnames = var.aws_vpc_enable_dns_hostnames
+  enable_dns_support   = var.aws_vpc_enable_dns_support
 
-  tags {
+  tags = {
     Name = "${var.aws_vpc_environment}-vpc"
   }
 }
 
 # Create the Internet Gateway
 resource "aws_internet_gateway" "vpc" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags {
+  tags = {
     Name = "${var.aws_vpc_environment}-igw"
   }
 }
 
 # Create a default Security Group
 resource "aws_default_security_group" "sg_vpc_default" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags {
+  tags = {
     Name = "${var.aws_vpc_environment}-vpc-default-sg"
   }
 }
+
